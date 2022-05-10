9df3458332c004f3a34a2eec0370c967eb4dd019
Emmanuel Marian Mathew <emmanuelmarianmat@gmail.com>
2022-05-11 04:01:58 +0530

    Revert "till 2.5"
    This reverts commit fdabe6041300b1812c26ca07494b52aceb147edc.

--- courseinfo/src/components/Total.js
+++
@@ -1,7 +0,0 @@
-const Total = (props) => {
-    return (
-        <strong>Number of exercises {props.parts.reduce((partialSum, part) => partialSum+part.exercises,0)}</strong>
-    )
-}
-
-export default Total;
